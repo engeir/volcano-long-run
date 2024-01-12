f014eaf06c061da946efbdc43fadc02902e92676
engeir <engeir@pm.me>
2024-01-12 11:14:13 +0100

    build(deps): add basic dependencies

---
+++ src/vlr/create/so2_injections.py
@@ -0,0 +1,9 @@
+"""Create a time series representing SO2 injections."""
+
+
+import volcano_cooking as vc
+
+
+def main() -> None:
+    """Run the main function."""
+    help(vc.synthetic_volcanoes.create)
